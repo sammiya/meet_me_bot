a09ccce39b72366476cfda9bf5450f14db3964d0
sammiya <338@tutanota.de>
2023-03-21 11:31:51 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# my-discord-reminder
+# meet-me-bot
+
+Discord の特定のチャンネルに予定を通知するものです。
+
+個人的な用途のために作成しているため、時刻が毎回 21 時に固定されているなど汎用的でない部分があります。
+
+ChatGPT の手を借りて作成しています。
+
+## 機能一覧
+
+- 特定のユーザーが特定のチャンネルで「次回 3/21」のように書くことで、予定として記憶しておく。（予定は 21 時固定。記憶できる日時はひとつまで）
+- 1 時間前および予定時刻には同チャンネルでお知らせをする。
+- 「次いつ？」のような文言を書きこむと次回の予定を教えてくれる。
+
+## install
+
+pip install -r requirements.txt
+
+## API キー取得方法
+
+1. Discord 開発者ポータルにアクセスし、Discord アカウントでログインする。
+
+2. 「New Application」をクリックして、新しいアプリケーションを作成する。
+
+3. 「General Information」ページで、アプリケーションの名前を入力して、「Create」をクリックする。
+
+4. 「Bot」タブを選択して、Bot アカウントを作成する。このときトークンを取得しておくこと。
+
+5. Bot の設定を変更し、必要に応じて Bot のアバターや名前を設定する。このとき Privileged Gateway Intents を全て有効にすること。（TODO: 不要なものもありそうなので検証）
+
+6. 「Oauth2」の「URL Generator」タブから、以下を選択して GERERATED URL をクリックし、bot をサーバーに追加するか聞かれるので「はい」を選択する。
+
+- SCOPES
+  - bot
+- GENERAL PERMISSIONS
+  - Read Messages/View Channels
+- TEXT PERMISSIONS
+  - Send Messages
+  - Read Message History
+  - Add Reactions
+
+## チャンネル ID 取得方法
+
+開発者モードをオンにした上でチャンネルを右クリックし、「ID をコピー」を選択
+
+## Discord の開発者モードをオンにする方法
+
+1. Discord アプリを開く。
+2. 左下の「ユーザー設定」アイコン（歯車）をクリックする。
+3. 「詳細設定」をクリックする。
+4. 「開発者モード」をオンにする。
+
+## sqlite3
+
+### インストール
+
+sudo apt-get install sqlite3
+
+### 接続
+
+sqlite3 meet-me-bot.db
+
+### スキーマ作成
+
+```
+CREATE TABLE meetings (
+    date TEXT NOT NULL,
+    pre_notification_sent INTEGER
+);
+```
---
+++ meet-me-bot.py
@@ -0,0 +1,111 @@
+import discord
+import os
+from dotenv import load_dotenv
+import datetime
+from discord.ext import commands, tasks
+import pytz
+import asyncio
+import sqlite3
+import locale
+
+from util import parse_next_date, is_asking_next_date
+
+# 日本語曜日名を定義
+WEEKDAYS = ['月', '火', '水', '木', '金', '土', '日']
+
+# .env ファイルから環境変数を読み込む
+load_dotenv()
+
+# Discord Bot トークンを取得する
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL'))
+MEETING_ANNOUNCER_ID = int(os.getenv('DISCORD_MEETING_ANNOUNCER'))
+
+# タイムゾーンを設定
+JST = pytz.timezone('Asia/Tokyo')
+
+intents = discord.Intents().all()  # すべてのイベントを有効化
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# SQLiteに接続
+conn = sqlite3.connect('meet-me-bot.db')
+cursor = conn.cursor()
+
+@tasks.loop(seconds=60)  # 60秒ごとに実行
+async def scheduled_message():
+    cursor.execute("SELECT date, pre_notification_sent FROM meetings")
+    row = cursor.fetchone()
+
+    if row is None:
+        return
+
+    scheduled_time_str, pre_notification_sent = row
+    scheduled_time = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').astimezone(JST)
+    now_time = datetime.datetime.now(JST)
+    delta_time = scheduled_time - now_time
+    remaining_seconds = delta_time.total_seconds()
+
+    if not pre_notification_sent and 3300 < remaining_seconds < 3660:
+        remaining_for_prenotification = remaining_seconds - 3600
+        await asyncio.sleep(remaining_for_prenotification)  # 差分ぶんだけ待機
+
+        channel = bot.get_channel(CHANNEL_ID)
+        await channel.send('1時間前だよ')
+        cursor.execute("UPDATE meetings SET pre_notification_sent = 1")
+        conn.commit()
+
+    if remaining_seconds < 60:
+        await asyncio.sleep(remaining_seconds)  # 差分ぶんだけ待機
+
+        channel = bot.get_channel(CHANNEL_ID)
+        await channel.send('はじまるよ')
+        cursor.execute("DELETE FROM meetings")
+        conn.commit()
+
+@bot.event
+async def on_message(message):
+    if is_asking_next_date(message.content):
+        channel = bot.get_channel(CHANNEL_ID)
+        cursor.execute("SELECT date FROM meetings")
+        row = cursor.fetchone()
+        if row is None:
+            await channel.send('次回予定は未定だよ')
+            return
+
+        scheduled_time_str = row[0]
+        scheduled_time = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S').astimezone(JST)
+        formatted = scheduled_time.strftime('%-m/%-d') + f'（{WEEKDAYS[scheduled_time.weekday()]}）'
+        await channel.send(f'次回予定: {formatted}')
+
+    if (message.author.id != MEETING_ANNOUNCER_ID):
+        return
+
+    parsed = parse_next_date(message.content)
+    if (parsed is None):
+        return
+
+    try:
+        conn.execute('BEGIN')
+        conn.execute('DELETE FROM meetings')
+        cursor.execute(f"INSERT INTO meetings(date, pre_notification_sent) VALUES ('{parsed.strftime('%Y-%m-%d %H:%M:%S')}', 0)")
+        conn.commit()
+        await message.add_reaction("👍")
+    except sqlite3.IntegrityError as e:
+        print(f"Error inserting parsed date: {e}")
+        await message.add_reaction("👎")
+        conn.rollback()
+
+@bot.event
+async def on_ready():
+    """
+    BotがDiscordにログインして準備ができたときに呼び出される関数。
+    ログインしたことを確認するために、Botの名前を出力し、
+    1回だけメッセージを送信するようにBotに指示する。
+    """
+    print(f'We have logged in as {bot.user}')
+    channel = bot.get_channel(CHANNEL_ID)
+    # await channel.send('Hello!')
+    scheduled_message.start()
+
+bot.run(TOKEN)
